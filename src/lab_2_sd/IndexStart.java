795db53b85eeafaa604e859acd6546e801b5dfd3
Nelson <Nelson@Nelson-Notebook>
2015-12-17 10:27:29 -0300

    lab 2 con captura de parametros desde archivo

--- src/lab_2_sd/IndexStart.java
+++ src/lab_2_sd/IndexStart.java
@@ -5,6 +5,10 @@
  */
 package lab_2_sd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Nelson
@@ -13,9 +17,44 @@ public class IndexStart {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        
+        // Lectura de parametros
+        
+        FileReader fr = new FileReader("IndexConf.ini");
+        
+        if(fr == null){
+            System.out.println("Archivo erroneo");
+            System.exit(1);
+        }
+        BufferedReader bf = new BufferedReader(fr);
+        String parametro;
+        int counter = 0;
+        int miPuerto;
+        int nParticiones;
+        
+        while( (parametro = bf.readLine() ) != null ){
+            switch (counter) {
+                case 0:
+                    miPuerto = Integer.parseInt(parametro);
+                    System.out.println("parametro " + counter + ": " + miPuerto);
+                    break;
+                case 1:
+                    nParticiones = Integer.parseInt(parametro);
+                    System.out.println("parametro " + counter + ": " + nParticiones);
+                    break;
+                default:
+                    System.out.println("parametro " + counter + ": " + parametro);
+                    break;
+            }
+            counter++;            
+        }
+        
+        // ************************************************ \\
+        
+        
     }
     
 }
